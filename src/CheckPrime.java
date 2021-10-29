d61fab9b1df432415230397a307a7f043caf1a9a
LamjingbaWaikhom <lamjingbawaikhom@gmail.com>
2021-10-29 12:38:22 +0530

    added new files

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -4,6 +4,8 @@ public class CheckPrime {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Prime Change");
+		System.out.println("Hello Prime Change Testing");
+
 	}
 
 }
---
+++ src/ExtraClass.java
@@ -0,0 +1,9 @@
+
+public class ExtraClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
